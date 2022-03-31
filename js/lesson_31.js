9ecacb701f166986761bcb9e307a7d7b239677c2
Rahimkhan <romakaibxanov@MacBook-Pro-Rahimkhan.local>
2022-03-31 23:59:21 +0400

    added lesson_35

---
+++ js/lesson_31.js
@@ -0,0 +1,24 @@
+
+    'use strict';
+
+    // Задача 1
+
+    // Переделайте этот код так, чтобы в нем операции инкремента и декремента:
+
+    // let num = 10;
+    // num = num + 1;
+    // num = num + 1;
+    // num = num - 1;
+    // alert(num); 
+
+    // let num = 10;
+    // num++;
+    // num++;
+    // num--;
+    // alert(num); 
+
+    let num1 = 3;
+    num1++;
+    let num2 = num1--;
+    alert(num1++);
+    alert(--num2);
---
+++ js/lesson_35.js
@@ -0,0 +1,55 @@
+
+    'use strict';
+
+    let num1 = 1;
+    let num2 = 2;
+    let num3 = num1 + num2;
+    console.log('сумма: ' + num3); 
+
+    let a = 1;
+    let b = 2;
+    console.log(a + b);
+
+    let num = '123';
+    let sum =  Number(num[0]) + Number(num[1]) + Number(num[2]);
+    console.log(sum);
+
+    let num4 = String(123);
+    console.log(num4[0]);
+
+    let a1 = 0;
+    console.log(++a1);
+
+    let num5 = 123;
+    console.log(String(num5).length);
+
+    let a2 = 24 * 60 * 60;
+    console.log(a2);
+
+    let num6 = 12345;
+    let str = String(num6);
+    console.log(str.length);
+
+    let num7 = 1238;
+    let str1 = String(num7);
+    console.log(str1[str1.length - 1]);
+
+    let num8 = 123;
+    let str2 = String(num8);
+    console.log(str2.length);
+
+    let num9 = 1237;
+    let str3 = String(num9);
+    console.log(str3[str3.length - 1]);
+
+    let a3 = '123';
+    let b1 = '456';
+    let s = Number(a3) + Number(b1);
+    
+    console.log(s);
+
+    let aaa = 1;
+    let bbb = 2;
+    let ccc = 3;
+    
+    console.log(aaa + bbb + ccc);
